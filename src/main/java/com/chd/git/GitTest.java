acc971d3e9e97939c7742b9587fb4fe46d95bb99
lkj <lkj@qq.com>
2021-12-21 13:07:17 +0800

    first commit

---
+++ src/main/java/com/chd/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.chd.git;
+
+/**
+ * @author lkj
+ * @creat 2021-12-21 12:57
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
