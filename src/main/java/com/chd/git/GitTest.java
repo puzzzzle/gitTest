b1ef1a37900c571c59e255b98a15de8a4542ccc4
lkj <lkj@qq.com>
2021-12-21 14:41:20 +0800

    master push test

--- src/main/java/com/chd/git/GitTest.java
+++ src/main/java/com/chd/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("hello git4!!!");
         System.out.println("master testMergeConflict");
         System.out.println("hot-fix testMergeConflict");
+        System.out.println("master push Commit");
 
     }
 }
